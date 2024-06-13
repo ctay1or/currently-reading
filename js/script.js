28e5acd675d19c3a408f42bf80583d1d92ae87a7
ctaylor <hi.ctaylor.co@gmail.com>
2024-06-12 19:05:32 -0500

    Added JS code

--- js/script.js
+++ js/script.js
@@ -5,5 +5,20 @@ const currentlyReading = {
   pages: 512,
   publicationDate: "August 4th, 2015",
   currentChapter: 8,
-  totalChapters: 23
+  totalChapters: 23,
 };
+
+console.log(currentlyReading.title);
+
+console.log(currentlyReading["pages"]);
+
+currentlyReading.awards = "Hugo Award for Best Novel (2016)"
+
+currentlyReading.updateChapter = function (chapterNum) {
+  this.currentChapter = chapterNum;
+};
+
+currentlyReading.updateChapter(11);
+
+console.log(currentlyReading);
+
